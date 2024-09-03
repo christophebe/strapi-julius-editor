ce6bd819a91bcf3b908522490eabb82c265cc826
devmoatassem <moatassemsindhu@gmail.com>
2024-09-03 09:06:58 +0500

    preparing for package publishing

--- admin/src/components/Wysiwyg/index.js
+++ admin/src/components/Wysiwyg/index.js
@@ -209,9 +209,16 @@ const WysiwygContent = ({
       preserveWhitespace: "full",
     },
     onUpdate(ctx) {
-      if (settings.other.saveJson) {
+      const newContent = settings.other.saveJson
+      ? JSON.stringify(ctx.editor.getJSON())
+      : ctx.editor.getHTML();
+
+    // Update the state if the content has actually changed
+    if (newContent !== currentContent) {
+      setCurrentContent(newContent);
+      // if (settings.other.saveJson) {
         onChange({
-          target: { name, value: JSON.stringify(ctx.editor.getJSON()) },
+          target: { name, value: newContent },
         });
       } else {
         onChange({ target: { name, value: ctx.editor.getHTML() } });
@@ -220,12 +227,11 @@ const WysiwygContent = ({
   });
 
   useEffect(() => {
-    if (editor === null) return;
-    if (currentContent === "") {
-      // Content can be 2 things: JSON or String. Be able to display both things.
+   // Check if the value prop differs from the current editor content
+   if (editor && value !== currentContent) {
 
       try {
-        // If content is saved as json, parse it
+         // If content is saved as JSON, parse it
         const json = JSON.parse(value);
         setCurrentContent(value);
         editor.commands.setContent(json, false);
@@ -235,7 +241,7 @@ const WysiwygContent = ({
         editor.commands.setContent(value, false);
       }
     }
-  }, [editor]);
+  }, [editor,value]);
 
   return (
     <Field required={required}>
